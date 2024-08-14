5c1eceb52f3ebfb404f88556ca9b0294bd91fe11
Qin <liuyuhan1523@gmail.com>
2024-08-15 00:49:48 +0200

    Update renderers.py

--- model_components/renderers.py
+++ model_components/renderers.py
@@ -230,6 +230,182 @@ class RGBRenderer(nn.Module):
         return rgb
 
 
+class BinaryRenderer(nn.Module):
+    """Standard volumetric rendering.
+
+    Args:
+        background_color: Background color as RGB. Uses random colors if None.
+    """
+
+    def __init__(self, background_color: BackgroundColor = "random") -> None:
+        super().__init__()
+        self.background_color: BackgroundColor = background_color
+
+    @classmethod
+    def combine_binary(
+        cls,
+        binary: Float[Tensor, "*bs num_samples 1"],#*bs num_samples 1[Qin]
+        weights: Float[Tensor, "*bs num_samples 1"],
+        background_color: BackgroundColor = "random",
+        ray_indices: Optional[Int[Tensor, "num_samples"]] = None,
+        num_rays: Optional[int] = None,
+    ) -> Float[Tensor, "*bs 1"]:#*bs 3[Qin]
+        """Composite samples along ray and render color image.
+        If background color is random, no BG color is added - as if the background was black!
+
+        Args:
+            rgb: binary for each sample
+            weights: Weights for each sample
+            background_color: Background color as RGB.
+            ray_indices: Ray index for each sample, used when samples are packed.
+            num_rays: Number of rays, used when samples are packed.
+
+        Returns:
+            Outputs binary values.
+        """
+        if ray_indices is not None and num_rays is not None:
+            # Necessary for packed samples from volumetric ray sampler
+            if background_color == "last_sample":
+                raise NotImplementedError("Background color 'last_sample' not implemented for packed samples.")
+            comp_rgb = nerfacc.accumulate_along_rays(
+                weights[..., 0], values=binary, ray_indices=ray_indices, n_rays=num_rays
+            )
+            accumulated_weight = nerfacc.accumulate_along_rays(
+                weights[..., 0], values=None, ray_indices=ray_indices, n_rays=num_rays
+            )
+        else:
+            comp_binary = torch.sum(weights * binary, dim=-2)
+            accumulated_weight = torch.sum(weights, dim=-2)
+        if BACKGROUND_COLOR_OVERRIDE is not None:
+            background_color = BACKGROUND_COLOR_OVERRIDE
+        if background_color == "random":
+            # If background color is random, the predicted color is returned without blending,
+            # as if the background color was black.
+            return comp_binary
+        elif background_color == "last_sample":
+            # Note, this is only supported for non-packed samples.
+            background_color = binary[..., -1, :]
+            #background_color = rgb[..., -1, :]
+        background_color = cls.get_background_color(background_color, shape=comp_binary.shape, device=comp_binary.device)
+
+        assert isinstance(background_color, torch.Tensor)
+        comp_binary = comp_binary + background_color * (1.0 - accumulated_weight)
+        return comp_binary 
+
+    @classmethod
+    def get_background_color(
+        cls, background_color: BackgroundColor, shape: Tuple[int, ...], device: torch.device
+    ) -> Union[Float[Tensor, "1"], Float[Tensor, "*bs 1"]]:#Float[Tensor, "3"], Float[Tensor, "*bs 3"][Qin]
+        """Returns the RGB background color for a specified background color.
+        Note:
+            This function CANNOT be called for background_color being either "last_sample" or "random".
+
+        Args:
+            background_color: The background color specification. If a string is provided, it must be a valid color name.
+            shape: Shape of the output tensor.
+            device: Device on which to create the tensor.
+
+        Returns:
+            Background color as RGB.
+        """
+        assert background_color not in {"last_sample", "random"}
+        assert shape[-1] == 1, "Background color must be binary."#assert shape[-1] == 3[Qin]
+        if BACKGROUND_COLOR_OVERRIDE is not None:
+            background_color = BACKGROUND_COLOR_OVERRIDE
+        if isinstance(background_color, str) and background_color in colors.COLORS_DICT:
+            background_color = torch.tensor([1.0 if background_color == "white" else 0.0])  # 修改: 二进制处理
+            #background_color = colors.COLORS_DICT[background_color][Qin]
+        assert isinstance(background_color, Tensor)
+
+        # Ensure correct shape
+        return background_color.expand(shape).to(device)
+
+    def blend_background(
+        self,
+        image: Tensor,
+        background_color: Optional[BackgroundColor] = None,
+    ) -> Float[Tensor, "*bs 1"]:#*bs 3[Qin]
+        """Blends the background color into the image if image is RGBA.
+        Otherwise no blending is performed (we assume opacity of 1).
+
+        Args:
+            image: RGB/RGBA per pixel.
+            opacity: Alpha opacity per pixel.
+            background_color: Background color.
+
+        Returns:
+            Blended RGB.
+        """
+        if image.size(-1) < 2:#< 4[Qin]
+            return image
+
+        binary, opacity = image[..., :1], image[..., 1:]#image[..., :3], image[..., 3:][Qin]
+        if background_color is None:
+            background_color = self.background_color
+            if background_color in {"last_sample", "random"}:
+                background_color = "black"
+        background_color = self.get_background_color(background_color, shape=binary.shape, device=binary.device)
+        assert isinstance(background_color, torch.Tensor)
+        return binary * opacity + background_color.to(binary.device) * (1 - opacity)
+
+    def blend_background_for_loss_computation(
+        self,
+        pred_image: Tensor,
+        pred_accumulation: Tensor,
+        gt_image: Tensor,
+    ) -> Tuple[Tensor, Tensor]:
+        """Blends a background color into the ground truth and predicted image for
+        loss computation.
+
+        Args:
+            gt_image: The ground truth image.
+            pred_image: The predicted RGB values (without background blending).
+            pred_accumulation: The predicted opacity/ accumulation.
+        Returns:
+            A tuple of the predicted and ground truth RGB values.
+        """
+        background_color = self.background_color
+        if background_color == "last_sample":
+            background_color = "black"  # No background blending for GT
+        elif background_color == "random":
+            background_color = torch.rand_like(pred_image)
+            pred_image = pred_image + background_color * (1.0 - pred_accumulation)
+        gt_image = self.blend_background(gt_image, background_color=background_color)
+        return pred_image, gt_image
+
+    def forward(
+        self,
+        binary: Float[Tensor, "*bs num_samples 1"],#*bs num_samples 3[Qin]
+        weights: Float[Tensor, "*bs num_samples 1"],
+        ray_indices: Optional[Int[Tensor, "num_samples"]] = None,
+        num_rays: Optional[int] = None,
+        background_color: Optional[BackgroundColor] = None,
+    ) -> Float[Tensor, "*bs 1"]:#*bs 3[Qin]
+        """Composite samples along ray and render color image
+
+        Args:
+            rgb: RGB for each sample
+            weights: Weights for each sample
+            ray_indices: Ray index for each sample, used when samples are packed.
+            num_rays: Number of rays, used when samples are packed.
+            background_color: The background color to use for rendering.
+
+        Returns:
+            Outputs of rgb values.
+        """
+
+        if background_color is None:
+            background_color = self.background_color
+
+        if not self.training:
+            binary = torch.nan_to_num(binary)
+        binary = self.combine_binary(
+            binary, weights, background_color=background_color, ray_indices=ray_indices, num_rays=num_rays
+        )
+        if not self.training:
+            torch.clamp_(rgb, min=0.0, max=1.0)
+        return binary # 返回的是二进制图像
+
 class SHRenderer(nn.Module):
     """Render RGB value from spherical harmonics.
 
